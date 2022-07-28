5b70d51f72266b7c715fdb5a6ab6d9c7ae455a9e
roshangotra12345 <roshan_jha@excellencetechnologies.in>
2022-07-28 16:48:55 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,35 @@
+require('./MongoDb/db')
+require('./MongoDb/models/Register')
+require("dotenv")
+  .config();
+
+const express =require('express')
+const app =express()
+const {json} = require('express')
+
+
+const cors = require('cors')
+
+
+
+const Router =require('./routes/route')
+
+const PORT = process.env.PORT || 8000
+
+
+
+app.use(cors())
+app.use(json())
+
+app.use(express.urlencoded({
+    extended: true
+  }));
+
+app.use(Router)
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`connection successfully on the ${PORT}`)
+})
